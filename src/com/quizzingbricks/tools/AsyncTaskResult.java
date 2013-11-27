a9adf0f2cef24f1a2dd3ff83babcfd41097dccbc
David Eriksson <david@UL30VT-13.10>
2013-11-27 15:12:22 +0100

    Added some boolean function to make the if statement nice looking

--- src/com/quizzingbricks/tools/AsyncTaskResult.java
+++ src/com/quizzingbricks/tools/AsyncTaskResult.java
@@ -1,21 +1,39 @@
 package com.quizzingbricks.tools;
 
 public class AsyncTaskResult<ResultType>        {
-        private ResultType result;
-        private Exception exception;
-        
-        public AsyncTaskResult(ResultType result){
-                this.result = result;
-        }
-        public AsyncTaskResult(Exception exception)        {
-                this.exception = exception;
-        }
-        
-        public ResultType getResult()        {
-                return this.result;
-        }
-        
-        public Exception getException()        {
-                return this.exception;
-        }
+    private ResultType result;
+    private Exception exception;
+    
+    public AsyncTaskResult(ResultType result){
+        this.result = result;
+    }
+    public AsyncTaskResult(Exception exception)        {
+        this.exception = exception;
+    }
+    
+    public ResultType getResult()        {
+        return this.result;
+    }
+    
+    public Exception getException()        {
+        return this.exception;
+    }
+    
+    public boolean hasException()	{
+    	if(this.exception == null)	{
+    		return false;
+    	}
+    	else	{
+    		return true;
+    	}
+    }
+    
+    public boolean hasResult()	{
+    	if(this.result == null)	{
+    		return false;
+    	}
+    	else	{
+    		return true;
+    	}
+    }
 }
