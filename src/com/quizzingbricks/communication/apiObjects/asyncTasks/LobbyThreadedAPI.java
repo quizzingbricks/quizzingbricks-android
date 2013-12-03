e228de2a52d47c9d48b4b0e160074184e1c19b20
David Eriksson <david@UL30VT-13.10>
2013-12-03 08:47:19 +0100

    Now compatible with REST API v0.1.5.7

--- src/com/quizzingbricks/communication/apiObjects/asyncTasks/LobbyThreadedAPI.java
+++ src/com/quizzingbricks/communication/apiObjects/asyncTasks/LobbyThreadedAPI.java
@@ -19,7 +19,7 @@ public class LobbyThreadedAPI extends AbstractThreadedAPI {
 	
 	public void createLobby(int size, OnTaskCompleteAsync onTaskCompleteClass)	{
 		postCall.addOnTaskComplete(onTaskCompleteClass);
-		postCall.addToTheEndOfUrl(serverLobbyApiPath + "/create/");
+		postCall.addToTheEndOfUrl(serverLobbyApiPath);
 		postCall.execute(new BasicNameValuePair("size", Integer.toString(size)));
 	}
 	
