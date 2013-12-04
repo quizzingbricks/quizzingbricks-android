c917d6975bb60188ec3bd64aa08430d8f722951d
David Eriksson <david@UL30VT-13.10>
2013-12-04 13:23:48 +0100

    Question promt now has a countdown timer

--- src/com/quizzingbricks/activities/FirstStartActivity.java
+++ src/com/quizzingbricks/activities/FirstStartActivity.java
@@ -1,5 +1,7 @@
 package com.quizzingbricks.activities;
 
+import java.io.IOException;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +12,7 @@ import com.quizzingbricks.R;
 
 import com.quizzingbricks.activities.menu.MainMenuActivity;
 
+import com.quizzingbricks.activities.answerQuestion.QuestionPromptActivity;
 import com.quizzingbricks.activities.gameboard.GameBoardActivity;
 
 import com.quizzingbricks.authentication.AuthenticationManager;
@@ -33,12 +36,12 @@ public class FirstStartActivity extends Activity {
 	public void changeToLoginActivity(View view)	{
 //		Intent intent = new Intent(this, LoginActivity.class);
 //		startActivity(intent);
-		startActivity(new Intent(this, GameBoardActivity.class));
+//		startActivity(new Intent(this, GameBoardActivity.class));
+		startActivity(new Intent(this, QuestionPromptActivity.class));
 	}
 	
 	public void changeToRegisterUserActivity(View view)	{
-//		Intent intent = new Intent(this, RegisterUserActivity.class);
-//		startActivity(intent);
-		startActivity(new Intent(this, GameBoardActivity.class));
+		Intent intent = new Intent(this, RegisterUserActivity.class);
+		startActivity(intent);
 	}
 }
